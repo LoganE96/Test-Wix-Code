dd487c0faed58977aba38d5980235dbde494a1c0
LoganE96 <123587531+LoganE96@users.noreply.github.com>
2026-09-16 11:45:00 -0600

    Update dynamic-chart.js
    Test import code replaced with the chart rendering code.

--- dynamic-chart.js
+++ dynamic-chart.js
@@ -1,4 +1,59 @@
+console.log("dynamic-chart.js loaded.");
 
-console.log("--------------------------------------");
-console.log("Github code is loaded and running.");
-console.log("--------------------------------------");
+class DynamicChart extends HTMLElement {
+
+	constructor() {
+		super();
+	}
+	
+	static get observedAttributes() {
+		return [
+			"chart-type",
+			"chart-data"
+		];
+	}
+
+	connectedCallback() {
+		console.log("Dynamic Chart connected.");
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		console.log("Attribute change detected.");
+		console.log("Received Attribute:", newValue);
+		
+		if (name === "chart-data") {
+			this.renderChart();
+		}
+	}
+
+	renderChart() {
+		const chartType = this.getAttribute("chart-type");
+
+		const chartData = JSON.parse(this.getAttribute("chart-data"));
+
+		if (!chartData) {
+			return;
+		}
+
+		if (chartType === "line") {
+			this.renderLineChart(chartData);
+		}
+
+		if (chartType === "pie") {
+			this.renderPieChart(chartData);
+		}
+	}
+
+	renderLineChart(data) {
+		// Chart.js line chart logic.
+	}
+
+	renderPieChart(data) {
+		// Chart.js pie chart logic.
+	}
+}
+
+customElements.define(
+	"dynamic-chart",
+	DynamicChart
+);
